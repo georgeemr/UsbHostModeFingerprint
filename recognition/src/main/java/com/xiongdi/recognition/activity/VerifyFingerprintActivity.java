a87a1a5ec1152333378445fdfcabeeffd6148b9f
CGmaybe10 <409958786@qq.com>
2016-06-01 14:30:06 +0800

    完成验证指纹流程

--- recognition/src/main/java/com/xiongdi/recognition/activity/VerifyFingerprintActivity.java
+++ recognition/src/main/java/com/xiongdi/recognition/activity/VerifyFingerprintActivity.java
@@ -6,9 +6,9 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,11 +18,12 @@ import android.widget.ImageView;
 import com.futronictech.AnsiSDKLib;
 import com.futronictech.UsbDeviceDataExchangeImpl;
 import com.xiongdi.recognition.R;
+import com.xiongdi.recognition.application.MainApplication;
 import com.xiongdi.recognition.util.ToastUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.ref.WeakReference;
 
 /**
@@ -32,16 +33,18 @@ import java.lang.ref.WeakReference;
 public class VerifyFingerprintActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = "moubiao";
 
-    private static final int MESSAGE_SHOW_MSG = 1;
     private static final int MESSAGE_SHOW_IMAGE = 2;
     private static final int MESSAGE_SHOW_ERROR_MSG = 3;
+
+    private final float MATCH_SCORE = 93.0f;
 
     private ImageView fingerIMG;
     private ImageButton takeBT;
     private Bitmap mFingerBitmap;
 
-    VerifyHandler mHandler;
     private UsbDeviceDataExchangeImpl usb_host_ctx;
+    private VerifyHandler mHandler;
+    private VerifyThread mVerifyThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,31 +85,48 @@ public class VerifyFingerprintActivity extends AppCompatActivity implements View
      * 验证指纹
      */
     private void verifyFingerprint() {
+        if (MainApplication.fingerprintPath == null) {
+            Log.e(TAG, "verifyFingerprint: fingerprint file path is null");
+            ToastUtil.getInstance().showToast(this, "fingerprint file path is null");
+            return;
+        }
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.e(TAG, "verifyFingerprint: external storage is not mounted");
+            ToastUtil.getInstance().showToast(this, "external storage is not mounted");
+            return;
+        }
         if (usb_host_ctx.OpenDevice(0, true)) {
-            byte[] templateContent = null;
-            String savePath = getExternalFilesDir(null).getPath() + File.separator + "null_5(ANSI)";
-            FileInputStream fs;
-            File f;
+            byte[] fingerprintContent = null;
+            File fingerprintFile;
+            FileInputStream fis = null;
             try {
-                f = new File(savePath);
-                if (!f.exists() || !f.canRead()) {
-                    throw new FileNotFoundException();
+                Log.d(TAG, "verifyFingerprint: fingerprint path " + MainApplication.fingerprintPath);
+                fingerprintFile = new File(MainApplication.fingerprintPath);
+                if (!fingerprintFile.exists() || !fingerprintFile.canRead()) {
+                    Log.e(TAG, "verifyFingerprint: fingerprint file no exist");
+                    return;
                 }
-                long nFileSize = f.length();
-                fs = new FileInputStream(f);
-                byte[] fileContent = new byte[(int) nFileSize];
-                fs.read(fileContent);
-                fs.close();
-                templateContent = fileContent;
+                fingerprintContent = new byte[(int) fingerprintFile.length()];
+                fis = new FileInputStream(fingerprintFile);
+                if (-1 == fis.read(fingerprintContent)) {
+                    Log.e(TAG, "verifyFingerprint: fingerprint content is null");
+                }
             } catch (Exception e) {
-                String error = String.format("Failed to load template from file %s. Error: %s.", "null_5(ANSI)", e.toString());
-                Log.d(TAG, "verifyFingerprint: " + error);
                 e.printStackTrace();
+            } finally {
+                try {
+                    if (fis != null) {
+                        fis.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
-            new VerifyThread(1, templateContent, 0).start();
+            mVerifyThread = new VerifyThread(0, fingerprintContent, MATCH_SCORE);
+            mVerifyThread.start();
         } else {
-            ToastUtil.getInstance().showToast(this, "open module failed!");
+            ToastUtil.getInstance().showToast(this, "open usb host mode failed!");
         }
     }
 
@@ -115,29 +135,29 @@ public class VerifyFingerprintActivity extends AppCompatActivity implements View
      */
     private class VerifyThread extends Thread {
         private AnsiSDKLib ansi_lib = null;
-        private int mFinger = 0;
-        private byte[] mTmpl = null;
-        private float mMatchScore = 0;
-        private boolean mCanceled = false;
+        private int fingerIndex = 0;//手指的编号
+        private byte[] templeData = null;//从文件里读取到的指纹数据
+        private float matchScore = 0;//匹配的标准值
+        private boolean cancel = false;
 
-        public VerifyThread(int finger, byte[] template, float matchScore) {
+        public VerifyThread(int fingerIndex, byte[] templeData, float matchScore) {
             ansi_lib = new AnsiSDKLib();
-            mFinger = finger;
-            mTmpl = template;
-            mMatchScore = matchScore;
-        }
-
-        public boolean isCanceled() {
-            return mCanceled;
+            this.fingerIndex = fingerIndex;
+            this.templeData = templeData;
+            this.matchScore = matchScore;
         }
 
         public void cancel() {
-            mCanceled = true;
+            cancel = true;
             try {
                 this.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+
+        public boolean isCanceled() {
+            return cancel;
         }
 
         @Override
@@ -147,34 +167,38 @@ public class VerifyFingerprintActivity extends AppCompatActivity implements View
                 //打开设备
                 if (!ansi_lib.OpenDeviceCtx(usb_host_ctx)) {
                     mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, ansi_lib.GetErrorMessage()).sendToTarget();
+                    Log.e(TAG, "run: open fingerprint device failed!");
                     return;
                 }
                 dev_open = true;
                 //计算指纹图像的大小
                 if (!ansi_lib.FillImageSize()) {
                     mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, ansi_lib.GetErrorMessage()).sendToTarget();
+                    Log.e(TAG, "run: get fingerprint image size failed!");
                     return;
                 }
                 byte[] img_buffer = new byte[ansi_lib.GetImageSize()];
                 //验证指纹
-                for (; ; ) {
+                while (true) {
+                    Log.d(TAG, "run: search fingerprint---->");
                     if (isCanceled()) {
                         break;
                     }
                     float[] matchResult = new float[1];
-                    if (ansi_lib.VerifyTemplate(mFinger, mTmpl, img_buffer, matchResult)) {
+                    if (ansi_lib.VerifyTemplate(fingerIndex, templeData, img_buffer, matchResult)) {
                         mFingerBitmap = createFingerBitmap(ansi_lib.GetImageWidth(), ansi_lib.GetImageHeight(), img_buffer);
                         mHandler.obtainMessage(MESSAGE_SHOW_IMAGE).sendToTarget();
-                        Log.d(TAG, "run: verify passed");
+                        boolean pass = matchResult[0] > matchScore;
+                        Log.d(TAG, "run: verify passed match result = " + matchResult[0] + " success = " + pass);
                         break;
                     } else {
-                        Log.e(TAG, "run: verify failed");
                         int lastError = ansi_lib.GetErrorCode();
                         if (lastError == AnsiSDKLib.FTR_ERROR_EMPTY_FRAME
                                 || lastError == AnsiSDKLib.FTR_ERROR_NO_FRAME
                                 || lastError == AnsiSDKLib.FTR_ERROR_MOVABLE_FINGER) {
                             Thread.sleep(100);
                         } else {
+                            Log.e(TAG, "run: verify failed");
                             String error = String.format("Verify failed. Error: %s.", ansi_lib.GetErrorMessage());
                             mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, error).sendToTarget();
                             break;
@@ -188,6 +212,44 @@ public class VerifyFingerprintActivity extends AppCompatActivity implements View
             //关闭模块
             if (dev_open) {
                 ansi_lib.CloseDevice();
+            }
+        }
+    }
+
+    private static class VerifyHandler extends Handler {
+        private WeakReference<VerifyFingerprintActivity> mWeakReference;
+
+        public VerifyHandler(VerifyFingerprintActivity activity) {
+            mWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            final VerifyFingerprintActivity activity = mWeakReference.get();
+            switch (msg.what) {
+                case MESSAGE_SHOW_ERROR_MSG:
+                    String showErr = (String) msg.obj;
+                    ToastUtil.getInstance().showToast(activity, showErr);
+                    break;
+                case MESSAGE_SHOW_IMAGE:
+                    activity.fingerIMG.setImageBitmap(activity.mFingerBitmap);
+                    break;
+                case UsbDeviceDataExchangeImpl.MESSAGE_ALLOW_DEVICE: {//同意使用usb设备的权限申请
+                    Log.d(activity.TAG, "handleMessage: allow device---->");
+                    if (activity.usb_host_ctx.ValidateContext()) {
+
+                    } else {
+                    }
+
+                    break;
+                }
+                case UsbDeviceDataExchangeImpl.MESSAGE_DENY_DEVICE: {//拒绝使用usb设备的权限申请
+                    Log.d(activity.TAG, "handleMessage: deny device---->");
+                    break;
+                }
+                default:
+                    break;
             }
         }
     }
@@ -222,43 +284,12 @@ public class VerifyFingerprintActivity extends AppCompatActivity implements View
         return result;
     }
 
-    private static class VerifyHandler extends Handler {
-        private WeakReference<VerifyFingerprintActivity> mWeakReference;
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
 
-        public VerifyHandler(VerifyFingerprintActivity activity) {
-            mWeakReference = new WeakReference<>(activity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            final VerifyFingerprintActivity activity = mWeakReference.get();
-            switch (msg.what) {
-                case MESSAGE_SHOW_MSG:
-                    String showMsg = (String) msg.obj;
-                    break;
-                case MESSAGE_SHOW_ERROR_MSG:
-                    String showErr = (String) msg.obj;
-                    ToastUtil.getInstance().showToast(activity, showErr);
-                    break;
-                case MESSAGE_SHOW_IMAGE:
-                    activity.fingerIMG.setImageBitmap(activity.mFingerBitmap);
-                    break;
-                case UsbDeviceDataExchangeImpl.MESSAGE_ALLOW_DEVICE: {
-                    if (activity.usb_host_ctx.ValidateContext()) {
-
-                    } else {
-                    }
-
-                    break;
-                }
-                case UsbDeviceDataExchangeImpl.MESSAGE_DENY_DEVICE: {
-                    break;
-                }
-                default:
-                    break;
-
-            }
+        if (mVerifyThread != null) {
+            mVerifyThread.cancel();
         }
     }
 }
