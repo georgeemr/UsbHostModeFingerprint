5536292a529b3ed473f9f6abc99e3722f913dbf0
CGmaybe10 <409958786@qq.com>
2016-06-01 10:56:35 +0800

    采集指纹界面完成

--- recognition/src/main/java/com/xiongdi/recognition/activity/GatherFingerprintActivity.java
+++ recognition/src/main/java/com/xiongdi/recognition/activity/GatherFingerprintActivity.java
@@ -1,5 +1,6 @@
 package com.xiongdi.recognition.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -7,12 +8,11 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -20,12 +20,13 @@ import android.widget.ImageView;
 import com.futronictech.AnsiSDKLib;
 import com.futronictech.UsbDeviceDataExchangeImpl;
 import com.xiongdi.recognition.R;
+import com.xiongdi.recognition.application.MainApplication;
 import com.xiongdi.recognition.util.ToastUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.util.Locale;
 
 /**
  * Created by moubiao on 2016/3/23.
@@ -33,13 +34,7 @@ import java.util.Locale;
  */
 public class GatherFingerprintActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = "moubiao";
-    private int KEY_CODE_RIGHT_BOTTOM = 249;
-    private int KEY_CODE_LEFT_BOTTOM = 250;
-    private int KEY_CODE_LEFT_TOP = 251;
-    private int KEY_CODE_RIGHT_TOP = 252;
-    private int KEY_CODE_FRONT_CAMERA = 27;//前置摄像头
 
-    private static final int MESSAGE_SHOW_MSG = 1;
     private static final int MESSAGE_SHOW_IMAGE = 2;
     private static final int MESSAGE_SHOW_ERROR_MSG = 3;
     private static final String kAnsiTemplatePostfix = "(ANSI)";
@@ -49,11 +44,13 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
     private ImageButton takeBT;
     private Bitmap mFingerBitmap;
 
-    FingerprintHandler mHandler;
+    private GatherFingerThread gatherThread;
+    private FingerprintHandler mHandler;
     private UsbDeviceDataExchangeImpl usb_host_ctx;
 
     private String gatherID;
     private int fingerNum;
+    private StringBuilder templeName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +69,11 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
         Intent data = getIntent();
         gatherID = data.getStringExtra("gatherID");
         fingerNum = data.getIntExtra("fingerNum", -1);
+        templeName = new StringBuilder();
+        templeName.append(gatherID);
+        templeName.append("_");
+        templeName.append(fingerNum);
+        templeName.append(kAnsiTemplatePostfix);
     }
 
     private void initView() {
@@ -88,7 +90,6 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
         switch (v.getId()) {
             case R.id.take_fingerprint_bt:
                 gatherFingerprint();
-
                 break;
             default:
                 break;
@@ -100,45 +101,41 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
      */
     private void gatherFingerprint() {
         if (usb_host_ctx.OpenDevice(0, true)) {
-            new GatherFingerThread(fingerNum, true, false, gatherID + "_" + fingerNum).start();
+            gatherThread = new GatherFingerThread(fingerNum, true, false);
+            gatherThread.start();
         } else {
-            ToastUtil.getInstance().showToast(this, "open module failed!");
+            ToastUtil.getInstance().showToast(this, "open usb host mode failed!");
         }
     }
-
 
     /**
      * 采集指纹的线程
      */
     private class GatherFingerThread extends Thread {
         private AnsiSDKLib ansi_lib = null;
-        private int mFinger = 0;
+        private int mFingerIndex = 0;
         private boolean mSaveAnsi = true;
         private boolean mSaveIso = false;
-        private String mTempleName = "";
-
-        public GatherFingerThread(int finger, boolean saveAnsi, boolean saveIso, String templeName) {
-            ansi_lib = new AnsiSDKLib();
-            mFinger = finger;
-            mSaveAnsi = saveAnsi;
-            mSaveIso = saveIso;
-            mTempleName = templeName;
-        }
-
         private boolean mCanceled = false;
 
-        public boolean isCanceled() {
-            return mCanceled;
+        public GatherFingerThread(int fingerIndex, boolean saveAnsi, boolean saveIso) {
+            ansi_lib = new AnsiSDKLib();
+            mFingerIndex = fingerIndex;
+            mSaveAnsi = saveAnsi;
+            mSaveIso = saveIso;
         }
 
         public void cancel() {
             mCanceled = true;
-
             try {
                 this.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+
+        public boolean isCanceled() {
+            return mCanceled;
         }
 
         @Override
@@ -148,17 +145,19 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
                 //打开设备
                 if (!ansi_lib.OpenDeviceCtx(usb_host_ctx)) {
                     mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, ansi_lib.GetErrorMessage()).sendToTarget();
+                    Log.e(TAG, "run: open fingerprint module failed " + ansi_lib.GetErrorMessage());
                     return;
                 }
                 dev_open = true;
                 //计算指纹图像的大小
                 if (!ansi_lib.FillImageSize()) {
                     mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, ansi_lib.GetErrorMessage()).sendToTarget();
+                    Log.e(TAG, "run: get image size failed " + ansi_lib.GetErrorMessage());
                     return;
                 }
                 byte[] img_buffer = new byte[ansi_lib.GetImageSize()];
                 //采集指纹
-                for (; ; ) {
+                while (true) {
                     if (isCanceled()) {
                         break;
                     }
@@ -169,35 +168,39 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
                     int[] realSize = new int[1];
                     int[] realIsoSize = new int[1];
 
-                    if (ansi_lib.CreateTemplate(mFinger, img_buffer, template, realSize)) {
+                    if (ansi_lib.CreateTemplate(mFingerIndex, img_buffer, template, realSize)) {//采集指纹成功
                         mFingerBitmap = createFingerBitmap(ansi_lib.GetImageWidth(), ansi_lib.GetImageHeight(), img_buffer);
                         mHandler.obtainMessage(MESSAGE_SHOW_IMAGE).sendToTarget();
 
                         //如果是ansi格式的直接保存
                         if (mSaveAnsi) {
-                            saveTemplate(mTempleName + kAnsiTemplatePostfix, template, realSize[0]);
+                            saveTemplate(template, realSize[0]);
                         }
-                        //如果是iso格式的先转换成ansi再保存
+                        //ansi格式转换为iso格式再保存
                         if (mSaveIso) {
                             realIsoSize[0] = templeMaxSize;
                             if (ansi_lib.ConvertAnsiTemplateToIso(template, templateIso, realIsoSize)) {
-                                saveTemplate(mTempleName + kIsoTemplatePostfix, templateIso, realIsoSize[0]);
+                                saveTemplate(templateIso, realIsoSize[0]);
                             } else {
                                 String error = String.format("iso Convert to ansi failed. Error: %s.", ansi_lib.GetErrorMessage());
                                 mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, error).sendToTarget();
+                                Log.e(TAG, "run: ansi convert to iso failed " + ansi_lib.GetErrorMessage());
                             }
                         }
+                        setResult(Activity.RESULT_OK);
 
                         break;
-                    } else {
+                    } else {//采集指纹失败
                         int lastError = ansi_lib.GetErrorCode();
                         if (lastError == AnsiSDKLib.FTR_ERROR_EMPTY_FRAME
                                 || lastError == AnsiSDKLib.FTR_ERROR_NO_FRAME
                                 || lastError == AnsiSDKLib.FTR_ERROR_MOVABLE_FINGER) {
                             Thread.sleep(100);
                         } else {
-                            String error = String.format("Create failed. Error: %s.", ansi_lib.GetErrorMessage());
+                            String error = String.format("gather fingerprint failed. Error: %s.", ansi_lib.GetErrorMessage());
                             mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, error).sendToTarget();
+                            Log.e(TAG, "run: gather fingerprint failed = " + ansi_lib.GetErrorMessage());
+                            setResult(Activity.RESULT_CANCELED);
                             break;
                         }
                     }
@@ -211,6 +214,8 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
             if (dev_open) {
                 ansi_lib.CloseDevice();
             }
+
+            finish();
         }
     }
 
@@ -247,46 +252,35 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
     /**
      * 保存模板
      *
-     * @param name     模板的名字
      * @param template 保存模板数据的数组（大于等于真实的模板数据）
      * @param size     真实的模板数据的大小
      */
-    private void saveTemplate(String name, byte[] template, int size) {
-        String savePath = getExternalFilesDir(null).getPath() + File.separator + name;
-        Log.d(TAG, "saveTemplate: file = " + savePath);
+    private void saveTemplate(byte[] template, int size) {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Log.e(TAG, "saveTemplate: external storage not mounted!");
+            return;
+        }
 
+        MainApplication.fingerprintPath = getExternalFilesDir(null) + File.separator + templeName;
         File saveFile;
-        FileOutputStream fs;
+        FileOutputStream fos = null;
         try {
-            saveFile = new File(savePath);
-            fs = new FileOutputStream(saveFile);
-
+            saveFile = new File(MainApplication.fingerprintPath);
+            fos = new FileOutputStream(saveFile);
             byte[] writeTemplate = new byte[size];
             System.arraycopy(template, 0, writeTemplate, 0, size);
-            fs.write(writeTemplate);
-            fs.close();
+            fos.write(writeTemplate);
         } catch (Exception e) {
-            String error = String.format("Failed to save template to file %s. Error: %s.", name, e.toString());
-            mHandler.obtainMessage(MESSAGE_SHOW_ERROR_MSG, -1, -1, error).sendToTarget();
             e.printStackTrace();
-        }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((KEY_CODE_LEFT_BOTTOM == keyCode || KEY_CODE_LEFT_TOP == keyCode
-                || KEY_CODE_RIGHT_BOTTOM == keyCode || KEY_CODE_RIGHT_TOP == keyCode)) {
-            gatherFingerprint();
-        }
-
-        if (KEY_CODE_FRONT_CAMERA == keyCode) {
-            if (event.getRepeatCount() == 25) {
-                gatherFingerprint();
-                return true;
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
-        return super.onKeyDown(keyCode, event);
     }
 
     private static class FingerprintHandler extends Handler {
@@ -301,9 +295,6 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
             super.handleMessage(msg);
             final GatherFingerprintActivity activity = mWeakReference.get();
             switch (msg.what) {
-                case MESSAGE_SHOW_MSG:
-                    String showMsg = (String) msg.obj;
-                    break;
                 case MESSAGE_SHOW_ERROR_MSG:
                     String showErr = (String) msg.obj;
                     ToastUtil.getInstance().showToast(activity, showErr);
@@ -311,21 +302,19 @@ public class GatherFingerprintActivity extends AppCompatActivity implements View
                 case MESSAGE_SHOW_IMAGE:
                     activity.fingerprintIMG.setImageBitmap(activity.mFingerBitmap);
                     break;
-                case UsbDeviceDataExchangeImpl.MESSAGE_ALLOW_DEVICE: {
-                    if (activity.usb_host_ctx.ValidateContext()) {
-
-                    } else {
-                    }
-
-                    break;
-                }
-                case UsbDeviceDataExchangeImpl.MESSAGE_DENY_DEVICE: {
-                    break;
-                }
                 default:
                     break;
-
             }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        //按返回键时表示取消指纹采集,如果采集成功会自动关闭该activity，不需要手动按返回键
+        if (gatherThread != null) {
+            gatherThread.cancel();
+        }
+        setResult(Activity.RESULT_CANCELED);
     }
 }
