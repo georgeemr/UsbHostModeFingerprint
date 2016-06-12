f920a9911ccbdc4d4e76cf288d3fdf9c60d9489c
CGmaybe10 <409958786@qq.com>
2016-06-12 10:40:37 +0800

    biometrics添加搜索view

---
+++ biometrics/src/main/java/com/xiongdi/biometrics/activity/MainActivity.java
@@ -0,0 +1,193 @@
+package com.xiongdi.biometrics.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.xiongdi.biometrics.R;
+import com.xiongdi.biometrics.widget.searchview.SearchAdapter;
+import com.xiongdi.biometrics.widget.searchview.SearchHistoryTable;
+import com.xiongdi.biometrics.widget.searchview.SearchItem;
+import com.xiongdi.biometrics.widget.searchview.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private SearchView mSearchView;
+    private SearchHistoryTable mHistoryDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mSearchView = (SearchView) findViewById(R.id.searchView);
+        setSearchView();
+        customSearchView();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            invalidateOptionsMenu();
+            mSearchView.setVisibility(View.VISIBLE);
+            mSearchView.open(false);
+
+            return true;
+        } else{
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void setSearchView() {
+        mHistoryDatabase = new SearchHistoryTable(this);
+
+        mSearchView = (SearchView) findViewById(R.id.searchView);
+        if (mSearchView != null) {
+            mSearchView.setVersion(SearchView.VERSION_MENU_ITEM);
+            mSearchView.setVersionMargins(SearchView.VERSION_MARGINS_MENU_ITEM);
+            mSearchView.setHint(R.string.app_name);
+            mSearchView.setTextSize(16);
+            mSearchView.setHint("Search");
+            mSearchView.setDivider(false);
+            mSearchView.setVoice(true);
+            mSearchView.setVoiceText("Set permission on Android 6+ !");
+            mSearchView.setAnimationDuration(SearchView.ANIMATION_DURATION);
+            mSearchView.setShadowColor(ContextCompat.getColor(this, R.color.search_shadow_layout));
+            mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    mHistoryDatabase.addItem(new SearchItem(query));
+//                    mSearchView.close(false);
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+            mSearchView.setOnOpenCloseListener(new SearchView.OnOpenCloseListener() {
+                @Override
+                public void onOpen() {
+                }
+
+                @Override
+                public void onClose() {
+                }
+            });
+
+            List<SearchItem> suggestionsList = new ArrayList<>();
+            suggestionsList.add(new SearchItem("search1"));
+            suggestionsList.add(new SearchItem("search2"));
+            suggestionsList.add(new SearchItem("search3"));
+
+            SearchAdapter searchAdapter = new SearchAdapter(this, suggestionsList);
+            searchAdapter.setOnItemClickListener(new SearchAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position) {
+                    mSearchView.close(false);
+                    TextView textView = (TextView) view.findViewById(R.id.textView_item_text);
+                    String query = textView.getText().toString();
+                }
+            });
+            mSearchView.setAdapter(searchAdapter);
+        }
+    }
+
+    protected void customSearchView() {
+//        final Bundle extras = getIntent().getExtras();
+//        if (extras != null) {
+//            mSearchView.setVersion(extras.getInt("version"));
+//            mSearchView.setVersionMargins(extras.getInt("version_margins"));
+//            mSearchView.setTheme(extras.getInt("theme"), true);
+//            mSearchView.setText(extras.getString("text"));
+
+            mSearchView.setVersion(SearchView.VERSION_MENU_ITEM);
+            mSearchView.setVersionMargins(SearchView.VERSION_MARGINS_MENU_ITEM);
+            mSearchView.setTheme(SearchView.THEME_LIGHT, true);
+            mSearchView.setText("test...");
+//        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
