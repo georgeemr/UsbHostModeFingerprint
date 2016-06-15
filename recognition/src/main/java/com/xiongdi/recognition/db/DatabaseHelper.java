8795ee8f85d0cfac0941fca76ed8566155a1ce41
CGmaybe10 <409958786@qq.com>
2016-06-15 15:30:20 +0800

    给person表添加新字段

--- recognition/src/main/java/com/xiongdi/recognition/db/DatabaseHelper.java
+++ recognition/src/main/java/com/xiongdi/recognition/db/DatabaseHelper.java
@@ -26,7 +26,7 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private Map<String, Dao> daoMap = new HashMap<>();
 
     private DatabaseHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION, R.raw.ormlite_config);
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     /**
