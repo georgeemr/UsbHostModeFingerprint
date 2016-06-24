607e1476a758f238e1e26ea826f4a811bddeed31
CGmaybe10 <409958786@qq.com>
2016-06-24 19:17:49 +0800

    采集指纹时添加动画

--- recognition/src/main/java/com/xiongdi/recognition/fragment/GatherFingerDialogFragment.java
+++ recognition/src/main/java/com/xiongdi/recognition/fragment/GatherFingerDialogFragment.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.xiongdi.recognition.R;
+import com.xiongdi.recognition.widget.progressBar.ProgressBarView;
 
 /**
  * Created by moubiao on 2016/6/23.
@@ -19,6 +20,7 @@ import com.xiongdi.recognition.R;
 public class GatherFingerDialogFragment extends DialogFragment implements View.OnClickListener {
     private GatherResultCallback mResultCallback;
     private ImageView fingerIMG;
+    private ProgressBarView gatherProView;
     Button saveBT, againBT;
 
     @Nullable
@@ -26,6 +28,7 @@ public class GatherFingerDialogFragment extends DialogFragment implements View.O
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View dialogLayout = inflater.inflate(R.layout.gather_fingerprint_pop_layout, container, false);
         fingerIMG = (ImageView) dialogLayout.findViewById(R.id.gather_fingerprint_img);
+        gatherProView = (ProgressBarView) dialogLayout.findViewById(R.id.gather_finger_progress);
         Button cancelBT = (Button) dialogLayout.findViewById(R.id.cancel_gather_bt);
         saveBT = (Button) dialogLayout.findViewById(R.id.save_gather_bt);
         saveBT.setEnabled(false);
@@ -51,10 +54,21 @@ public class GatherFingerDialogFragment extends DialogFragment implements View.O
                 dismiss();
                 break;
             case R.id.gather_again_bt:
+                showGatherProgress(true);
                 mResultCallback.gatherAgain();
                 break;
             default:
                 break;
+        }
+    }
+
+    public void showGatherProgress(boolean show) {
+        if (show) {
+            fingerIMG.setVisibility(View.GONE);
+            gatherProView.setVisibility(View.VISIBLE);
+        } else {
+            fingerIMG.setVisibility(View.VISIBLE);
+            gatherProView.setVisibility(View.GONE);
         }
     }
 
@@ -63,6 +77,7 @@ public class GatherFingerDialogFragment extends DialogFragment implements View.O
     }
 
     public void setFingerprint(Bitmap bitmap) {
+        showGatherProgress(false);
         fingerIMG.setImageBitmap(bitmap);
         saveBT.setEnabled(true);
         againBT.setEnabled(true);
